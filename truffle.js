18074bad6d3856d7b8854b3be2690e55581664fd
swissarmykirpan <gurpreet@gurpreetsohal.com>
2017-10-20 00:34:47 +0100

    deploy to ropsten configuration

--- truffle.js
+++ truffle.js
@@ -1,3 +1,27 @@
+var bip39 = require("bip39");
+var hdkey = require('ethereumjs-wallet/hdkey');
+var ProviderEngine = require("web3-provider-engine");
+var WalletSubprovider = require('web3-provider-engine/subproviders/wallet.js');
+var Web3Subprovider = require("web3-provider-engine/subproviders/web3.js");
+var Web3 = require("web3");
+const FilterSubprovider = require('web3-provider-engine/subproviders/filters.js');
+
+var mnemonic = "couch solve unique spirit wine fine occur rhythm foot feature glory away";
+var hdwallet = hdkey.fromMasterSeed(bip39.mnemonicToSeed(mnemonic));
+
+var wallet_hdpath = "m/44'/60'/0'/0/";
+var wallet = hdwallet.derivePath(wallet_hdpath + "0").getWallet();
+var address = "0x" + wallet.getAddress().toString("hex");
+
+var providerUrl = "https://ropsten.infura.io/DelxQHmlbFFpgk8ZbiH6";
+var engine = new ProviderEngine();
+// filters
+engine.addProvider(new FilterSubprovider());
+
+engine.addProvider(new WalletSubprovider(wallet, {}));
+engine.addProvider(new Web3Subprovider(new Web3.providers.HttpProvider(providerUrl)));
+engine.start(); // Required by the provider engine.
+
 module.exports = {
   networks: {
     development: {
@@ -6,10 +30,9 @@ module.exports = {
       network_id: "*" // Match any network id,
     },
     ropsten:  {
-      host: "localhost",
-      port: 8545,
-      from: '0x8e3e2ae91926146a932acb242fc8d3041da97e73',
-      network_id: "*" // Match any network id,
+      network_id: 3,    // Official ropsten network id
+      provider: engine, // Use our custom provider
+      from: address     // Use the address we derived
     }
   }
 };
